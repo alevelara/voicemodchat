c38abec5d1a7fd9eb9afccab0467c3b3bccd4d9d
Alejandro <alevelara@gmail.com>
2021-06-08 17:50:49 +0200

    - Adding squeleton

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace voicemodchat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
